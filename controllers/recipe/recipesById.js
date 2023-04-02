5485a2b44000b4d332c543eab0aa79fcfde99301
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-04-02 14:10:46 +0300

    Update recipesById.js

--- controllers/recipe/recipesById.js
+++ controllers/recipe/recipesById.js
@@ -4,15 +4,15 @@ const { HttpError } = require("../../helpers");
 
 const recipesById = async (request, response) => {
   const { id } = request.params;
-  const result = await recipe.findById(id);
-  if (!result) {
+  const recipes = await recipe.findById(id);
+  if (!recipes) {
     throw HttpError(404, "Not found");
   }
   response.json({
     status: "success",
     code: 200,
     data: {
-      result,
+      recipes,
     },
   });
 };
