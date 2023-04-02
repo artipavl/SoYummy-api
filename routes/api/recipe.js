f85143eddd825107b563753f0b8cf1b6fae0b6c8
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-04-02 14:37:03 +0300

    Update recipe.js

--- routes/api/recipe.js
+++ routes/api/recipe.js
@@ -9,10 +9,10 @@ router.get("/", recipe.allRecipes);
 
 router.get("/main-page", recipe.recipesMainPage);
 
-router.get("/:id", isValidId, recipe.recipesById);
-
 router.get("/list/:category", recipe.recipesByCategory);
 
 router.get("/category-list", recipe.categoryList);
+
+router.get("/:id", isValidId, recipe.recipesById);
 
 module.exports = router;
