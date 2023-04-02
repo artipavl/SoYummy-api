5c40b2aef33ab954568a0215a053286ad6901db3
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-04-02 10:22:12 +0300

    fix recipesMainPage

--- controllers/recipe/recipesMainPage.js
+++ controllers/recipe/recipesMainPage.js
@@ -1,7 +1,49 @@
 const { recipe } = require("../../models");
 
-const recipesMainPage = async (request, response) => {
+// const recipesMainPage = async (request, response) => {
 
+//   const CATEGORIES = [
+//     "Beef",
+//     "Breakfast",
+//     "Chicken",
+//     "Dessert",
+//     "Goat",
+//     "Lamb",
+//     "Miscellaneous",
+//     "Pasta",
+//     "Pork",
+//     "Seafood",
+//     "Side",
+//     "Starter",
+//     "Vegan",
+//     "Vegetarian",
+//   ];
+
+//   const DISH_PER_CATEGORY = 4;
+
+//   const dishesByCategory = {};
+//   let dishesArray = [];
+
+//   for (const category of CATEGORIES) {
+//     const dishes = await recipe.find({ category });
+//     if (dishes) {
+//       dishesArray = dishes.slice(0, Math.min(dishes.length, DISH_PER_CATEGORY));
+//     } else {
+//       dishesArray = [];
+//     }
+//     dishesByCategory[category] = dishesArray;
+//   }
+
+//   response.json({
+//     status: "success",
+//     code: 200,
+//     data: {
+//       dishesByCategory,
+//     },
+//   });
+// };
+
+const recipesMainPage = async (request, response) => {
   const CATEGORIES = [
     "Beef",
     "Breakfast",
@@ -19,26 +61,24 @@ const recipesMainPage = async (request, response) => {
     "Vegetarian",
   ];
 
-  const DISH_PER_CATEGORY = 4;
+  const dishesArray = [];
 
-  const dishesByCategory = {};
-  let dishesArray = [];
+  const skip = 0;
+  const limit = 4;
 
   for (const category of CATEGORIES) {
-    const dishes = await recipe.find({ category });
-    if (dishes) {
-      dishesArray = dishes.slice(0, Math.min(dishes.length, DISH_PER_CATEGORY));
-    } else {
-      dishesArray = [];
-    }
-    dishesByCategory[category] = dishesArray;
+    const dishes = await recipe.find({ category }, "-createdAt -updatedAt", {
+      skip,
+      limit,
+    });
+    dishesArray.push({ category, recipes: dishes });
   }
 
   response.json({
     status: "success",
     code: 200,
     data: {
-      dishesByCategory,
+      recipes: dishesArray,
     },
   });
 };
