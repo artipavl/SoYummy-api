524cfe9ce8d31081c851baaa5aafd3fa6a822bb9
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-04-04 22:57:52 +0300

    Update deleteOwnRecipe.js

--- controllers/recipe/deleteOwnRecipe.js
+++ controllers/recipe/deleteOwnRecipe.js
@@ -1,23 +1,21 @@
-const { recipe } = require('../../models');
 const { HttpError } = require("../../helpers");
+const { recipe } = require("../../models");
 
 const deleteOwnRecipe = async (request, response) => {
-    const { id } = request.params;
-    const { _id: owner } = request.user;
-    if (!owner) {
-      throw HttpError(404, "Not found the owner");
-    }
-    const result = await recipe.findByIdAndRemove(id);
-    response.json({
-      status: "success",
-      code: 200,
-      message: "contact deleted",
-      data: {
-        result,
-      },
-    });
+  const { id } = request.params;
+  const { _id: owner } = request.user;
+  const result = await recipe.findOneAndRemove({ _id: id, owner });
+  if (result === null) {
+    throw HttpError(404, "Not found");
+  }
+  response.json({
+    status: "success",
+    code: 200,
+    message: "contact deleted",
+    data: {
+      result,
+    },
+  });
 };
 
-module.exports = deleteOwnRecipe
-
-
+module.exports = deleteOwnRecipe;
