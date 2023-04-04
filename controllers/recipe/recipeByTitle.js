31f0253aa68b9456c54cb3e8c6e36c23cb3c358c
Kristina Ra4kovi4 <103757850+Ra4kovi4@users.noreply.github.com>
2023-04-04 16:42:32 +0300

    fix favorites, fix achievement, add total in get controllers,add authentificate to router recipe

--- controllers/recipe/recipeByTitle.js
+++ controllers/recipe/recipeByTitle.js
@@ -3,24 +3,33 @@ const { recipe } = require("../../models");
 const { HttpError } = require("../../helpers");
 
 const recipeByTitle = async (req, res) => {
-  const { title } = req.params;
-  const { page = 1, limit = 10 } = req.query;
-  const skip = (page - 1) * limit;
-  const result = await recipe.find(
-    { title: { $regex: title } },
-    "-createdAt -updatedAt",
-    { skip, limit }
-  );
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  res.json({
-    status: "success",
-    code: 200,
-    data: {
-      result,
-    },
-  });
+	const { title } = req.params;
+	const { page = 1, limit = 10 } = req.query;
+
+	const skip = (page - 1) * limit;
+	const result = await recipe.find(
+		{ title: { $regex: title } },
+		"-createdAt -updatedAt",
+		{
+			skip,
+			limit,
+		}
+	);
+	const total = await recipe
+		.find({ title: { $regex: title } })
+		.countDocuments();
+
+	if (!result) {
+		throw HttpError(404, "Not found");
+	}
+	res.json({
+		status: "success",
+		code: 200,
+		data: {
+			total,
+			result,
+		},
+	});
 };
 
 module.exports = recipeByTitle;
--- controllers/recipe/recipesByIngredient.js
+++ controllers/recipe/recipesByIngredient.js
@@ -3,20 +3,20 @@ const { recipe } = require("../../models");
 const { HttpError } = require("../../helpers");
 
 const recipesByIngredient = async (req, res) => {
-  const { ingredientId} = req.params;
+	const { ingredientId } = req.params;
 
- 
-  const result = await recipe.find({ "ingredient.id": ingredientId });
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  res.json({
-    status: "success",
-    code: 200,
-    data: {
-      result
-    },
-  });
+	const result = await recipe.find({ "ingredient.id": ingredientId });
+	if (!result) {
+		throw HttpError(404, "Not found");
+	}
+	res.json({
+		status: "success",
+		code: 200,
+		data: {
+			total: result.length,
+			result,
+		},
+	});
 };
 
 module.exports = recipesByIngredient;
