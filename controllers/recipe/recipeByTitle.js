6a5875dd687dff1b7cd16c59d7e880d4cdf06b1a
Kristina Ra4kovi4 <103757850+Ra4kovi4@users.noreply.github.com>
2023-04-05 20:32:10 +0300

    add indredients to recipeById  and listIngridients controller

--- controllers/recipe/recipeByTitle.js
+++ controllers/recipe/recipeByTitle.js
@@ -3,29 +3,30 @@ const { recipe } = require("../../models");
 const { HttpError } = require("../../helpers");
 
 const recipeByTitle = async (req, res) => {
-  const { title } = req.query;
-  const { page = 1, limit = 6 } = req.query;
-  const skip = (page - 1) * limit;
-  const result = await recipe.find(
-    { title: { $regex: title } },
-    "-createdAt -updatedAt",
-    { skip, limit }
-  );
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  const total = await recipe
+	const { title } = req.query;
+
+	const { page = 1, limit = 6 } = req.query;
+	const skip = (page - 1) * limit;
+	const result = await recipe.find(
+		{ title: { $regex: title, $options: "i" } },
+		"-createdAt -updatedAt",
+		{ skip, limit }
+	);
+	if (!result) {
+		throw HttpError(404, "Not found");
+	}
+	const total = await recipe
 		.find({ title: { $regex: title } })
 		.countDocuments();
-    
-  res.json({
-    status: "success",
-    code: 200,
-    data: {
-      total,
-      result,
-    },
-  });
+
+	res.json({
+		status: "success",
+		code: 200,
+		data: {
+			total,
+			result,
+		},
+	});
 };
 
 module.exports = recipeByTitle;
