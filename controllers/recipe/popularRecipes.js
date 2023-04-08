528bd40f286075351cf9676a7c51505684183e1f
Andrew Lischuk <66631681+AndrewLischuk@users.noreply.github.com>
2023-04-08 13:08:11 +0300

    rewrited popular recipes (agregation)

--- controllers/recipe/popularRecipes.js
+++ controllers/recipe/popularRecipes.js
@@ -3,18 +3,37 @@ const {
 } = require("../../models");
 
 const popularRecipes = async (req, res) => {
-  const result = await Recipe.find(
-    {},
+  const result = await Recipe.aggregate([
     {
-      title: 1,
-      description: 1,
-      preview: 1,
-      favorites: 1,
-    }
-  );
-  result
-    .sort((a, b) => b.favorites.length - a.favorites.length)
-    .splice(4, result.length);
+      $project: {
+        title: 1,
+        description: 1,
+        preview: 1,
+        favorites: 1,
+        favorites_count: { $size: { $ifNull: ["$favorites", []] } },
+      },
+    },
+    { $sort: { favorites_count: -1 } },
+  ]).limit(4);
+
+  // const result = await Recipe.find(
+  //   {},
+  //   {
+  //     title: 1,
+  //     description: 1,
+  //     preview: 1,
+  //     favorites: 1,
+  //   },
+  //   {
+  //     sort: {
+  //       popularity: -1,
+  //     },
+  //     limit: 4,
+  //   }
+  // );
+  // result
+  //   .sort((a, b) => b.favorites.length - a.favorites.length)
+  //   .splice(4, result.length);
 
   res.json({
     status: "success",
