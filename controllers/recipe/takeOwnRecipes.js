7862e466199bd3766057fba6876025d51d0ac6fc
vitaliy197811 <vital197811@gmail.com>
2023-04-15 21:12:29 +0300

    page+limit

--- controllers/recipe/takeOwnRecipes.js
+++ controllers/recipe/takeOwnRecipes.js
@@ -4,8 +4,10 @@ const {
 
 const takeOwnRecipes = async (req, res) => {
   const { _id: owner } = req.user;
+  const { page = "1", limit = "10" } = req.query;
+  const skip = (page - 1) * limit;
 
-  const result = await Recipe.find({ owner }, "-createdAt -updatedAt");
+  const result = await Recipe.find({ owner }, "-createdAt -updatedAt", { skip, limit });
 
   res.json({
     status: "success",
